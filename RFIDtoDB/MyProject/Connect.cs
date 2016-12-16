da6daef81fa507d1ac4a766c8f50c70b8feaf3fe
alanmdz <alanmdz@users.noreply.github.com>
2016-12-16 01:27:38 -0600

    RFID to Database
    RFID to Database Portion of the whole system.

---
+++ RFIDtoDB/MyProject/Connect.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace MyProject
+{
+    class Connect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //constructor
+        public Connect()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "storedata";
+            uid = "tester";
+            password = "password";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server");
+                        break;
+                    case 1045:
+                        MessageBox.Show("Invalid Username/password");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public void Insert()
+        {
+            string query = "INSERT INTO Department(DepartmentID, name) VALUES (4000000, 'Kids')";
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                cmd.ExecuteNonQuery();
+
+                this.CloseConnection();
+            }
+        }
+
+        public void Update()
+        {
+            string query = "UPDATE Item SET cost = '99.99' WHERE tag = 643242";
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand();
+
+                cmd.CommandText = query;
+
+                cmd.Connection = connection;
+
+                cmd.ExecuteNonQuery();
+
+                this.CloseConnection();
+            
+            }
+
+        }
+
+        public void Delete()
+        {
+            string query = "DELETE FROM Item WHERE tag = 123456";
+
+            if(this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+    }
+}
---
+++ RFIDtoDB/MyProject/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MyProject
+{
+    public partial class Form1 : Form
+    {
+        private Connect myConn;
+        public Form1()
+        {
+            InitializeComponent();
+
+            myConn = new Connect();
+        }
+
+        //inserts Kids Department
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myConn.Insert();
+        }
+
+        //updates George Foreman Grill to 99.99
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myConn.Update();
+        }
+
+        //deletes item "Anker iPhone Charger"
+        private void button3_Click(object sender, EventArgs e)
+        {
+            myConn.Delete();
+        }
+
+    }
+
+   
+}
